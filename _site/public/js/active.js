1a44636225cf82989de34b2fd6d341e476144870
codesyariah122 <pujiermanto@gmail.com>
2020-08-26 17:21:01 +0700

    Add script

---
+++ _site/public/js/active.js
@@ -0,0 +1,25 @@
+function activeNav(base){
+	 let navbar = document.getElementsByClassName('sidebar-nav-item');
+        for(let i=0; i<=navbar.length-1; i++){
+           let item = document.getElementsByClassName('sidebar-nav-item')[i].getAttribute('href');
+           let url = location.href;
+           let baseUrl = base;
+
+        if(url == baseUrl+item){
+            document.getElementsByClassName('sidebar-nav-item')[i].classList.add('active');
+        }
+    }
+}
+
+
+
+// const header = document.getElementById("navbar");
+// const btns = header.getElementsByClassName("sidebar-nav-item");
+// alert(btns.lengt);
+// for (let i = 0; i < btns.length; i++) {
+//   btns[i].addEventListener("click", function() {
+//     let current = document.getElementsByClassName("active");
+//         current[0].className = current[0].className.replace(" active", "");
+//         this.className += " active";
+//   });
+// }
