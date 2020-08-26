2a5c7d8a8ba9719008865fd519510c7bbfc12c31
codesyariah122 <pujiermanto@gmail.com>
2020-08-26 19:36:06 +0700

    🍳

--- _site/public/js/active.js
+++ _site/public/js/active.js
@@ -4,9 +4,7 @@ function activeNav(base){
            let item = document.getElementsByClassName('sidebar-nav-item')[i].getAttribute('href');
            let url = location.href;
            let baseUrl = base;
-           
            console.log(url);
-
         if(url == baseUrl+item){
         	console.log(url+" : "+baseUrl+item);
             document.getElementsByClassName('sidebar-nav-item')[i].classList.add('active');
