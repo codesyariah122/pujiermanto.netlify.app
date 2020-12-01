9a46d2249b083df88e2c22cf1de0b428fe4fe471
codesyariah122 <pujiermanto@gmail.com>
2020-12-01 16:52:35 +0700

    update image background with image background plugin

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navigation from './../components/navbar/navbar'
+
+const AboutPage = () => {
+    return (
+        <div>
+            <></>
+            <Navigation/>
+            <h1>Hallo World</h1>
+        </div>
+        
+    )
+}
+
+export default AboutPage
---
+++ src/pages/portfolio/avatar.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import styled from 'styled-components'
+import Img from 'gatsby-image'
+import { graphql, useStaticQuery } from 'gatsby'
+
+const AvatarWrap = styled.div`
+margin: auto 0;
+flex: 0 1 36px;
+@media (max-width: 768px) and (orientation: landscape) {
+    flex: 0 1 25px;
+}
+`
+
+const Avatar = () => {
+    const data = useStaticQuery(graphql`
+        query {
+            file: file(relativePath: { eq: "portfolio/avatar1.jpg" }) {
+                childImageSharp {
+                    fluid(maxWidth: 400, maxHeight: 500) {
+                    ...GatsbyImageSharpFluid
+                    }
+                }
+            }
+        }
+    `)
+
+    return (
+        <AvatarWrap>
+            <Img fluid={data.file.childImageSharp.fluid} alt="avatar"/>
+        </AvatarWrap>
+    )
+}
+
+export default Avatar
