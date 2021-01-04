89001ba8b2aec1fccef59a8fd3fcbb683d45b446
codesyariah122 <pujiermanto@gmail.com>
2021-01-04 23:00:36 +0700

    edit styled

--- src/pages/contact/contactpage.js
+++ src/pages/contact/contactpage.js
@@ -5,6 +5,22 @@ import Container from '@material-ui/core/Container';
 import Button from '@material-ui/core/Button';
 import {ContainerStyle, HeadingStyle} from './style'
 
+const ContainerStyle = styled.div`
+@import url('https://fonts.googleapis.com/css2?family=Lobster&family=Source+Code+Pro&family=Yellowtail&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Poiret+One&display=swap');
+font-family: 'Source Code Pro', monospace;
+	display: block
+	width: 100%;
+	height: auto;
+	margin-top: 3rem;
+`
+
+const HeadingStyle = styled.h1`
+	font-family: 'Poiret One', cursive;
+	font-size: 1.7rem;
+	color: #000;
+`
+
 const ContactPages = () => {
 	return (
 		<ContainerStyle>
