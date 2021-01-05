913dff0c6d6d10a3a76d2c92f230b709cd6d67fb
codesyariah122 <pujiermanto@gmail.com>
2021-01-05 22:55:21 +0700

    delete export styled

--- src/pages/contact/contactpage.jsx
+++ src/pages/contact/contactpage.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import styled from 'styled-components'
 import {graphql, useStaticQuery} from 'gatsby'
 import { Container } from '../../components/common';
 import contact from '../../images/animated/contactpage.gif';
@@ -7,7 +8,51 @@ import ContactForm from './ContactForm';
 
 
 const ContactPages = () => {
-	
+	const Wrapper = styled.div`
+	@import url('https://fonts.googleapis.com/css2?family=Lobster&family=Source+Code+Pro&family=Yellowtail&display=swap');
+	@import url('https://fonts.googleapis.com/css2?family=Poiret+One&display=swap');
+	font-family: 'Source Code Pro', monospace;
+	  padding: 4rem 0;
+	  display: flex;
+	  align-items: flex-start;
+	  justify-content: space-between;
+	  @media (max-width: 960px) {
+	    flex-direction: column;
+	  }
+	`;
+	const Details = styled.div`
+	  flex: 1;
+	  padding-right: 2rem;
+	  @media (max-width: 960px) {
+	    padding-right: unset;
+	    width: 100%;
+	    order: 1;
+	  }
+	  h1 {
+	    margin-bottom: 2rem;
+	    font-size: 26pt;
+	    color: #212121;
+	  }
+	  p {
+	    margin-bottom: 2.5rem;
+	    font-size: 20pt;
+	    font-weight: normal;
+	    line-height: 1.3;
+	    color: #707070;
+	  }
+	`;
+
+	const Thumbnail = styled.div`
+	  flex: 1;
+	  @media (max-width: 960px) {
+	    width: 100%;
+	    margin-bottom: 2rem;
+	  }
+	  img {
+	    width: 100%;
+	  }
+	`;
+
 	const data = useStaticQuery(graphql`
 		query {
 			site {
