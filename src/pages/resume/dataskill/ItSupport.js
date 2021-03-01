4bc9025a227b73b548d0ab2dd8240fc070e2c6ac
codesyariah122 <pujiermanto@gmail.com>
2021-03-01 08:24:01 +0700

    update resume

--- src/pages/resume/dataskill/ItSupport.js
+++ src/pages/resume/dataskill/ItSupport.js
@@ -52,8 +52,8 @@ const ItSupport = () => {
 					<ul style={{listStyle: 'none'}}>
 						<li>
 							{fetch.name}
-							<div classNameName="w3-light-grey w3-round-xlarge w3-small">
-							   	<div classNameName={`w3-container w3-center w3-round-xlarge w3-${fetch.color}`} style={{width: `${fetch.percent}%`}}>
+							<div className="w3-light-grey w3-round-xlarge w3-small">
+							   	<div className={`w3-container w3-center w3-round-xlarge w3-${fetch.color}`} style={{width: `${fetch.percent}%`}}>
 							         {fetch.percent}%
 							    </div>
 							</div>
